2b967bf1878de8bc572e8100d0f39f5d42891287
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-03-07 17:49:36 -0300

    feat: cria projeto git vuejs

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const message = defineModel('message', {default: "Hello world"})
+</script>
+
+<template>
+    <div id="letsGo">
+      <h1>Meu primeiro componente</h1>
+      <div>
+          Dizendo olá, para variar: {{ message }}
+      </div>
+      <input v-model="message" type="text">
+    </div>
+</template>
+
+<style scoped>
+  #letsGo {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+  }
+</style>
