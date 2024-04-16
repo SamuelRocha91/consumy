23f0a00ad510964928fca103710d27072ab84435
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-16 13:58:22 -0300

    feat: atualiza welcomeview

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,32 +1,10 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'
-import { auth } from '../auth';
-import { ref } from 'vue';
-const currentUser = ref(auth.currentUser())
-const signOut = function () {
-  auth.signOut(() => {
-    currentUser.value = auth.currentUser()
-  })
- }
+import UserProfile from '../components/UserProfile.vue'
 </script>
 
 <template>
   <main>
-    <h1>Delivery: Welcome</h1>
-    <template v-if="currentUser">
-      <h3>Hi, {{ currentUser.email }}</h3>
-      <br />
-      <nav>
-        <a @click="signOut">Sign Out</a>
-      </nav>
-    </template>
-    <template v-else>
-      <nav>
-        <RouterLink :to="{ name: 'signIn'}">
-          Sign In
-        </RouterLink>
-      </nav>
-    </template>
+    <UserProfile />
   </main>
 </template>
 
