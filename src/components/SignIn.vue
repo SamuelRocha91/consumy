2143e9237888e7d2e3311e0424c39fad6b43d8c9
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-04-18 08:47:57 -0300

    refatora lógica do SIgnIN

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,33 +1,42 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import { auth } from '../auth'
-const router = useRouter();
-const email = defineModel<string>('email');
-const password = defineModel<string>('password');
-const awaiting = ref(false);
+import { Auth } from '../auth'
+
+const router = useRouter()
+const email = defineModel<string>('email')
+const password = defineModel<string>('password')
+const awaiting = ref(false)
+const remember = defineModel<boolean>('remember', { default: true })
 
 function onSubmit() {
-    awaiting.value = true
-    auth.signIn(email.value || '', password.value || '', () => {
-        awaiting.value = false
-        router.push('/')
-    }, () => {
-        awaiting.value = false
-        console.log('não foi dessa vez!')
-    })
+  let auth = new Auth(remember.value)
+  awaiting.value = true
+  auth.signIn(
+    email.value || '',
+    password.value || '',
+    () => {
+      awaiting.value = false
+      router.push('/')
+    },
+    () => {
+      awaiting.value = false
+      console.log('não foi dessa vez!')
+    }
+  )
 }
 </script>
 <template>
-    <div>
-        <h1>Sign In</h1>
-        <form @submit.prevent="onSubmit">
-            <label>E-Mail: </label>
-            <input v-model="email" type="email"><br />
-            <label>Senha: </label>
-            <input v-model="password" type="password"><br />
-            <button type="submit" v-show="!awaiting">
-                Sign In</button>
-        </form>
-    </div>
+  <div>
+    <h1>Sign In</h1>
+    <form @submit.prevent="onSubmit">
+      <label>E-Mail: </label>
+      <input v-model="email" type="email" /><br />
+      <label>Senha: </label>
+      <input v-model="password" type="password" /><br />
+      <label>Remember Me: </label>
+      <input v-model="remember" type="checkbox" /><br />
+      <button type="submit" v-show="!awaiting">Sign In</button>
+    </form>
+  </div>
 </template>
