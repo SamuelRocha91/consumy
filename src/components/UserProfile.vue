f33725390b0ed82a30a17ef7bf93ba9885ab1322
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-05-25 22:44:05 -0300

    feat: estiliza página inicial da consumy

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -20,11 +20,55 @@ const signOut = function () {
       </nav>
     </template>
     <template v-else>
-      <h3>Por favor, faça login para ter acesso</h3>
-      <br />
       <nav>
-        <RouterLink :to="{name: 'signIn'}"> Sign In </RouterLink>
+        <RouterLink :to="{name: 'signIn'}">Login</RouterLink>
+        <RouterLink :to="{name: 'signIn'}">Registro</RouterLink>
       </nav>
+      <section class="food-text">
+          <h3>Comida boa e gostozinha, quem preparou foi a vovozinha</h3>
+      </section>
     </template>
   </main>
 </template>
+
+<style scoped>
+h3 {
+  color: white;
+  font-size: 32px;
+  font-weight: bold;
+}
+
+.food-text {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  align-content: flex-start;
+  text-align: center;
+  border: 1px solid white;
+    background-color: black;
+
+}
+
+nav {
+  display: flex;
+  width: 100vw;
+  justify-content: flex-end;
+  gap: 35px;
+  color: white;
+  padding: 35px;
+}
+
+nav a {
+  border: 1px solid white;
+  padding: 15px;
+  text-decoration: none;
+  color: white;
+  font-weight: bold;
+  border-radius: 15px;
+}
+
+nav a:hover {
+  background-color: rgb(0, 0, 0);
+  transition: 5s;
+}
+</style>
