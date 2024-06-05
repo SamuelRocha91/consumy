a1564a998e1bb332f0cfdda587fead50d0de42c7
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-05 14:22:57 -0300

    feat: torna navBar elemento geral de acesso a todas as rotas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import NavBar from '@/components/NavBar.vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
 </script>
 
 <template>
+  <NavBar v-if="route.path !== '/signUp' && route.path !== '/signIn'" :quantity="0"/>
   <RouterView />
 </template>
