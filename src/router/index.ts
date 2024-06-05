6396202c131676374f452da061298230bae2325c
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-05 14:37:59 -0300

    feat: insere página de not found pra casos em que inexiste página

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,7 @@ import StoreView from '@/views/StoreView.vue';
 import StoreDetailsView from '@/views/StoreDetailsView.vue';
 import CartView from '@/views/CartView.vue';
 import { Auth } from '../utils/auth'; 
+import NotFoundView from '@/views/NotFoundView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -53,7 +54,11 @@ const router = createRouter({
       path: '/dashboard/cart',
       component: CartView,
       name: 'cart',
-    }
+    },
+    {
+      path: '/:catchAll(.*)',
+      component: NotFoundView,
+    },
   ]
 });
 
