b771692f0470a2f40ef0fb1af1d9f22bb334014d
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-06 21:55:05 -0300

    feat: declara novas rotas de pedidos

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,7 @@ import CartView from '@/views/CartView.vue';
 import { Auth } from '../utils/auth'; 
 import NotFoundView from '@/views/NotFoundView.vue';
 import PaymentView from '@/views/PaymentView.vue';
+import OrderView from '@/views/OrderView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -55,6 +56,16 @@ const router = createRouter({
       path: '/dashboard/cart',
       component: CartView,
       name: 'cart',
+    },
+    {
+      path: '/dashboard/orders',
+      component: OrderView,
+      name: 'order',
+    },
+    {
+      path: '/dashboard/orders/:id',
+      component: OrderView,
+      name: 'order',
     },
     {
       path: '/dashboard/payment',
