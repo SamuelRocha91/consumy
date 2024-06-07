e7fc44460fe6a3c71061741de7cbf4d3fb6e544e
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-06-06 21:55:33 -0300

    feat: cria Order de visualização de pedidos e cars

---
+++ src/components/OrderCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+<template>
+    <div class="col-lg-4 col-md-6 order-card">
+        <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Pedido #12345</h5>
+              <h6 class="card-subtitle mb-2 text-muted">Data: 01/06/2024</h6>
+              <p class="card-text">Status: Em Processamento</p>
+              <p class="card-text">Total: R$ 130,00</p>
+              <a href="#" class="card-link">Ver Detalhes</a>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/OrderView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import OrderCard from '@/components/OrderCard.vue';
+</script>
+<template>
+  <div class="container">
+    <h1 class="my-4">Meus Pedidos</h1>
+    <div class="row">
+      <OrderCard />            
+    </div>
+  </div>
+</template>
